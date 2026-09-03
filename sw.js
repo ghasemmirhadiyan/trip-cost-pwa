1ccc39fec65ef34971475c03305eec9c338fdf51
Ghasem Mirhadiyan <87271837+ghasemmirhadiyan@users.noreply.github.com>
2026-09-03 18:22:55 +0330

    Add files via upload
    15.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='trip-pwa-v14-6';
+const CACHE='trip-pwa-v15-1';
 const ASSETS=['./','./index.html','./styles.css?v=15.0','./supabase-config.js?v=15.0','./auth.js?v=15.0','./app.js?v=15.0','./manifest.webmanifest','./assets/north-login-bg.png','./icons/icon.svg','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
